9b0507ca1f8da4c4ffbac196078506b59ecc74b9
Prajwal  Gawande <112270849+prajwalsggithub@users.noreply.github.com>
2025-04-29 15:38:48 +0530

    Update notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,1 +1,19 @@
 
+const express = require('express');
+const notesRouter = express.Router();
+const { createNote } = require('../controller/createNote');
+const { getAllNotes, getAllNotesById } = require('../controller/getNotes');
+const {updateNotes} = require('../controller/updateNotes');
+
+// Test route
+notesRouter.get('/working', (req, res) => {
+  res.status(200).send('Notes Home');
+});
+
+// Notes routes
+notesRouter.post('/notes', createNote);
+notesRouter.get('/notes', getAllNotes);
+notesRouter.get('/notesid/:id', getAllNotesById);
+notesRouter.get('/update', updateNotes);
+
+module.exports = notesRouter;

80a61866437d6119c6bb77378c60cef312b9a6d7
Prajwal  Gawande <112270849+prajwalsggithub@users.noreply.github.com>
2025-04-29 15:31:28 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,1 +1,13 @@
 
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+exports.connect = () =>{
+    mongoose.connect(process.env.MONGO_URL)
+    .then(()=>console.log("Database Connection Successfull ..")
+    )
+    .catch((err)=>{
+        console.log(err);
+        process.exit(1)
+    })
+}

569264d2414a2625f78ec3c9722883a7c47ae310
Prajwal  Gawande <112270849+prajwalsggithub@users.noreply.github.com>
2025-04-29 15:34:24 +0530

    Update createNote.js

--- controller/createNote.js
+++ controller/createNote.js
@@ -1,1 +1,34 @@
 
+const Note = require('../models/Note');
+exports.createNote = async(req,res) =>{
+    try {
+        const {title,content} = req.body;
+
+        const newNotes = await  Note.create({
+            title,
+            content
+        })
+        
+        if(!newNotes){
+            return res.status(200).json({
+                success:false,
+                message:'Notes not created',
+            })
+        }
+
+        return res.status(200).json({
+            success:true,
+            message:'Notes Created successfully',
+            data:newNotes
+        })
+
+    } catch (err) {
+        res.status(500).json({
+            success:false,
+            message:"Internal Server Error",
+            error:err.message,
+
+        })
+    }
+
+}

a80b224a72a89ffa5b7b335df729385ee116acc7
Prajwal  Gawande <112270849+prajwalsggithub@users.noreply.github.com>
2025-04-29 15:40:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+const express = require('express');
+const app = express();
+
+// Load environment variables
+require('dotenv').config();
+
+// MongoDB connection
+require('./config/database').connect();
+
+// Router import
+const notesRouter = require('./routes/notes');
+
+// Set up the port
+const port = process.env.PORT || 4000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api', notesRouter);
+
+// Test route
+app.get('/', (req, res) => {
+    res.send("WORKING ..")
+});
+
+// Start the server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
